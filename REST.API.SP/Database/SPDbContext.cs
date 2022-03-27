633b384bcaa635f96ed0cf5e93e2a6b70454637b
filip1998 <filip.mihic2@gmail.com>
2022-03-27 22:18:02 +0200

    DbContext

---
+++ REST.API.SP/Database/SPDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace REST.API.SP.Database
+{
+    public class SPDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+            => optionBuilder.UseNpgsql(@"Host=localhost;Port=5432;Database=OP;Username=postgres;Password=postgres");
+
+        public PDbContext()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public SbSet<Employees> Employees { get; set; }
+        public SbSet<Departments> Departments { get; set; }
+        public SbSet<Records> Records { get; set; }
+        public SbSet<TypeOfHours> TypeOfHours { get; set; }
+        public DbSet<WorkHourPrices> WorkHourPrices { get; set; }
+
+    }
+}
